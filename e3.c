4325e2f11b3831e1a2005584f5732a31415ae4f4
Selina-1111 <2307748453@qq.com>
2026-09-17 22:57:39 +0800

    e3.c

--- e3.c
+++ e3.c
@@ -3,7 +3,12 @@ int main()
 {
     double a;
     double b;
+    double c;
     scanf("%lf,%lf",&a,&b);
-    printf("%f,%f",b,a);
+    c=a;
+    a=b;
+    b=c;
+
+    printf("%f,%f",a,b);
     return 0;
 }
