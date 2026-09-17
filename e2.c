c7dd952d2b53a64ae9d56a4f34af0ba743baa7c3
Selina-1111 <2307748453@qq.com>
2026-09-17 22:22:34 +0800

    e2.c

--- e2.c
+++ e2.c
@@ -3,10 +3,10 @@
 int main()
 {
     system("chcp 65001");
-    double a;
-    double b;
-    scanf("%lf,%lf",&a,&b);
-    double c = (a + b)/2;
-    printf("%f与%f的平均值是%f",a,b,c);
+    int a;
+    int b;
+    scanf("%d,%d",&a,&b);
+    double c = (a + b)/2.0;
+    printf("%d与%d的平均值是%f",a,b,c);
     return 0;
 }
