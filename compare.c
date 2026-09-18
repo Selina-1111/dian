dfd5e146cf4b513d24d54d536acf7d77432df1a6
Selina-1111 <2307748453@qq.com>
2026-09-18 22:30:12 +0800

    compare.c

--- compare.c
+++ compare.c
@@ -4,9 +4,11 @@ int main()
     int a;
     int b;
     scanf("%d,%d",&a,&b);
-    int max = a;
+    int max;
     if(b>a){
         max = b;
+    }else{
+max=a;
     }
     printf("%d",max);
     return 0;
