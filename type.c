4e581c47694ab9f64e454b3408d57aa2c2f63734
Selina-1111 <2307748453@qq.com>
2026-09-19 13:27:45 +0800

    type.c

--- type.c
+++ type.c
@@ -3,14 +3,15 @@ int main()
 {
     int type;
     scanf("%d",&type);
-    if(type==1){
-        printf("i");
-    }
-    else if(type==2){
-        printf("love");
-    }
-    else if(type==3){
-        printf("u");
+    switch(type){
+        case 1:
+        printf("a");
+        break;
+        case 2:
+        printf("b");
+        break;
+        default:
+        printf("c");
     }
     return 0;
 }
