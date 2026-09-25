b6fe03ceaa11dde4058d82cc86416e36b949312c
Selina-1111 <2307748453@qq.com>
2026-09-25 16:32:58 +0800

    meiju.c

--- meiju.c
+++ meiju.c
@@ -2,15 +2,23 @@
 int main(){
     int i;
     int a;
+    int m=0;
     int b;
     for(i=1;i<=20;i++){
         for(b=1;b<=50;b++){
             for(a=1;a<=100;a++){
                 if(5*i+2*b+a==100){
                     printf("%d,%d,%d",i,a,b);
-                    goto out;
+                    m=1;
+                    break;
                 }
+
             }
+            if(m==1){
+                break;
+            }
+        }if(m==1){
+            break;
         }
     }
     out:
