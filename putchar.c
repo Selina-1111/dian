3d122b4558af42002f7a3975c0eb569ffe13590e
Selina-1111 <2307748453@qq.com>
2026-09-26 16:48:14 +0800

    putchar.c

---
+++ putchar.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int ch;//getchar的逻辑是拿到字符转为ascii码，是数字
+    while((ch=getchar())!=-1){
+        putchar(ch);//while循环让它不断地一个一个字符往外吐
+    }
+    return 0;
+}//没有回车垃圾？因为每一次putchar都顺势回车了
