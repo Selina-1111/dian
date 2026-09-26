65b5f159e0b65849b83bc534fe6e549f15ff0ae4
Selina-1111 <2307748453@qq.com>
2026-09-26 19:10:14 +0800

    dian.c

--- dian.c
+++ dian.c
@@ -10,21 +10,39 @@ int main(){
         {"001","cola",3.50},
         {"002","lollipop",0.50},
         {"003","noodles",6.00},
-    };
-    printf("please input the code:\n" );
-    char input[20];
-    scanf("%s",input);
-    int i;
-    int signal=0;
-    for(i=0;i<3;i++){
-        if(strcmp(input,item[i].code)==0){
+    };        
+     char input[20];
+     scanf("%s",input);
+     int situation;
+     if(strcmp(input,"exit")==0){
+        situation = 3;
+     }else if(strcmp(input,"quit")==0){
+        situation =3;
+     }else if(strcmp(input,"price")==0){
+        situation =2;
+     }else{
+        situation =1;
+     }
+    switch(situation){
+        case 1:
+        int i;
+        int signal=0;
+        for(i=0;i<3;i++){
+            if(strcmp(input,item[i].code)==0){
             printf("%s,%.2f\n",item[i].name,item[i].price);
             signal =1;
             break;
         }
     }
-    if(signal==0){
-        printf("not found");
+        if(signal==0){
+            printf("ERROR:code not found");
+    }
+        case 2:
+        for(i=0;i<3;i++){
+            printf("%s,%s,%.2f\n",item[i].code,item[i].name,item[i].price);
+        }
+        case 3:
+        printf("Bye~");
     }
     return 0;
 }
