b270168c6445c9471eff54c73717f171b70afc69
Selina-1111 <2307748453@qq.com>
2026-09-26 16:48:38 +0800

    putchar.c

--- putchar.c
+++ putchar.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     int ch;//getchar的逻辑是拿到字符转为ascii码，是数字
-    while((ch=getchar())!=-1){
+    while((ch=getchar())!=EOF){
         putchar(ch);//while循环让它不断地一个一个字符往外吐
     }
     return 0;
